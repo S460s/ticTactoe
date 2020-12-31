c1c01ee91478377815e929ed10f13cccdb86ea0d
S460 <aleksandar.ta6ev@gmail.com>
2020-12-31 12:30:20 +0200

    Changed position of removing the event listeners

--- script.js
+++ script.js
@@ -1,121 +1,115 @@
 const GameBoard = (function () {
-  let gameBoardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  let squares = document.querySelectorAll("#square")
+	let gameBoardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+	let squares = document.querySelectorAll("#square");
 
-  const displayBoard = function () {
-    squares.forEach(function (square) {
-      square.textContent = gameBoardArray[square.getAttribute("name")]
-    })
-  }
+	const displayBoard = function () {
+		squares.forEach(function (square) {
+			square.textContent = gameBoardArray[square.getAttribute("name")];
+		});
+	};
 
-  return { displayBoard, gameBoardArray, squares }
-})()
-
+	return { displayBoard, gameBoardArray, squares };
+})();
 
 const Player = function (sign) {
+	const removeElistener = () => {
+		GameBoard.squares.forEach((square) => {
+			square.removeEventListener("click", playEvent);
+		});
+	};
 
-  const removeElistener = () => {
-    GameBoard.squares.forEach(square => {
-      square.removeEventListener("click", playEvent)
-    })
-  }
+	const playEvent = function (e) {
+		if (e.target.textContent !== "x" && e.target.textContent !== "o") {
+			GameBoard.gameBoardArray[e.target.getAttribute("name")] = sign;
+			GameBoard.displayBoard();
+			e.target.style.cssText = "font-size: 20vh";
+			removeElistener();
+			GameFlow.game();
+		}
+	};
 
-  const playEvent = function (e) {
-    if (e.target.textContent !== "x" && e.target.textContent !== "o") {
-      GameBoard.gameBoardArray[e.target.getAttribute("name")] = sign
-      GameBoard.displayBoard()
-      e.target.style.cssText = "font-size: 20vh"
-      removeElistener()
-      GameFlow.game()
-    }
-  }
+	const play = function () {
+		GameBoard.squares.forEach(function (square) {
+			square.addEventListener("click", playEvent);
+		});
+	};
 
-  const play = function () {
-    GameBoard.squares.forEach(function (square) {
-      square.addEventListener("click", playEvent)
-    })
-  }
-
-  return { play, removeElistener }
-}
+	return { play, removeElistener };
+};
 
 const GameFlow = (function () {
-  const winMsg = document.getElementById("winMsg")
-  const playAgainButton = document.getElementById("playAgain")
-  const playerX = Player("x")
-  const playerO = Player("o")
+	const winMsg = document.getElementById("winMsg");
+	const playAgainButton = document.getElementById("playAgain");
+	const playerX = Player("x");
+	const playerO = Player("o");
 
-  const checkWinner = function (sign = "z") {
-    let strBoard = GameBoard.gameBoardArray.join("")
-    const re1 = RegExp(`${sign}..${sign}..${sign}`)
-    const re2 = RegExp(`${sign}...${sign}...${sign}`)
-    const re3 = RegExp(`..${sign}.${sign}.${sign}..`)
-    const re4 = strBoard.slice(3, 6) == `${sign}${sign}${sign}`
-    const re5 = strBoard.slice(0, 3) == `${sign}${sign}${sign}`
-    const re6 = strBoard.slice(6, 9) == `${sign}${sign}${sign}`
-    if (re1.test(strBoard) || re2.test(strBoard) || re3.test(strBoard) || re4 || re5 || re6) {
-      return "win";
-    }
-    else if (!GameBoard.gameBoardArray.includes(" ")) {
-      return "Tie"
-    }
-    else {
-      return false
-    }
-  }
+	const checkWinner = function (sign = "z") {
+		let strBoard = GameBoard.gameBoardArray.join("");
+		const re1 = RegExp(`${sign}..${sign}..${sign}`);
+		const re2 = RegExp(`${sign}...${sign}...${sign}`);
+		const re3 = RegExp(`..${sign}.${sign}.${sign}..`);
+		const re4 = strBoard.slice(3, 6) == `${sign}${sign}${sign}`;
+		const re5 = strBoard.slice(0, 3) == `${sign}${sign}${sign}`;
+		const re6 = strBoard.slice(6, 9) == `${sign}${sign}${sign}`;
+		if (re1.test(strBoard) || re2.test(strBoard) || re3.test(strBoard) || re4 || re5 || re6) {
+			return "win";
+		} else if (!GameBoard.gameBoardArray.includes(" ")) {
+			return "Tie";
+		} else {
+			return false;
+		}
+	};
 
-  const winAnimation = function () {
-    GameBoard.squares.forEach(function (square) {
-      winMsg.style.cssText = "transform: scale(1.2,1.2);"
-    })
-  }
+	const winAnimation = function () {
+		GameBoard.squares.forEach(function () {
+			playerX.removeElistener();
+			playerO.removeElistener();
+			winMsg.style.cssText = "transform: scale(1.2,1.2);";
+		});
+	};
 
-  const game = function () {
-    if (checkWinner() === "Tie") {
-      winMsg.textContent = "Tie!"
-      winAnimation()
-    }
+	const game = function () {
+		if (checkWinner() === "Tie") {
+			winMsg.textContent = "Tie!";
+			winAnimation();
+		}
 
-    playerX.play()
-    if (checkWinner("x") === "win") {
-      winMsg.textContent = "Player with Xs win!"
-      playerX.removeElistener()
-      playerO.removeElistener()
-      winAnimation()
-    }
-    else {
-      playerO.play()
-      if (checkWinner("o") === "win") {
-        winMsg.textContent = "Player with Os win!"
-        playerX.removeElistener()
-        playerO.removeElistener()
-        winAnimation()
-      }
-    }
-  }
-  const playAgain = function () {
-    playAgainButton.addEventListener("click", function () {
-      for (let i = 0; i < 9; i++) {
-        GameBoard.gameBoardArray[i] = " "
-      }
-      GameBoard.displayBoard()
-      winMsg.textContent = ""
+		playerX.play();
+		if (checkWinner("x") === "win") {
+			winMsg.textContent = "Player with Xs win!";
 
-      GameBoard.squares.forEach(function (square) {
-        square.style.cssText = ""
-      })
-      winMsg.style.cssText = ""
+			winAnimation();
+		} else {
+			playerO.play();
+			if (checkWinner("o") === "win") {
+				winMsg.textContent = "Player with Os win!";
+				winAnimation();
+			}
+		}
+	};
+	const playAgain = function () {
+		playAgainButton.addEventListener("click", function () {
+			for (let i = 0; i < 9; i++) {
+				GameBoard.gameBoardArray[i] = " ";
+			}
+			GameBoard.displayBoard();
+			winMsg.textContent = "";
 
-      game()
-    })
-  }
-  return { game, playAgain }
-})()
+			GameBoard.squares.forEach(function (square) {
+				square.style.cssText = "";
+			});
+			winMsg.style.cssText = "";
+
+			game();
+		});
+	};
+	return { game, playAgain };
+})();
 
 window.onload = function () {
-  document.querySelector("main").classList.add("board")
-  document.querySelector("h1").classList.add("title")
-}
+	document.querySelector("main").classList.add("board");
+	document.querySelector("h1").classList.add("title");
+};
 
-GameFlow.playAgain()
-GameFlow.game()
+GameFlow.playAgain();
+GameFlow.game();
